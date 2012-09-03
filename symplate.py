206c8ad4febbbd335f03d67a10e73cebc0541aef
Ben Hoyt <benhoyt@gmail.com>
2012-09-03 12:28:21 +1200

    Added README

--- symplate.py
+++ symplate.py
@@ -1,59 +1,6 @@
 """Symplate, the Simple pYthon teMPLATE renderer.
-=================================================
 
-Usage
------
-
-* TODO
-
-
-Filters
--------
-
-* TODO: discuss filt (not called filter due to builtin) and get_default_filter
-
-
-Unicode handling
-----------------
-
-* TODO
-
-
-Comments
---------
-
-* TODO: {% # foo %}, and note that you can't do {% for x in y: # comment %}
-
-
-Outputting a literal {{
------------------------
-
-You can't include {{ or }} anywhere inside an output expression, and you can't
-include {% or %} anywhere inside a code block. To output a literal {{, }}, {%,
-or %}, use Python's string literal concatenation so the two {'s are separated.
-For example, this will output a single {{:
-
-    {{ '{' '{' }}
-
-If you find yourself needing this a lot (for instance in writing a template
-about templates), you could shortcut and name it at the top of your template:
-
-    {% LB, RB = '{' '{', '}' '}' %}
-    {{LB}}one{{RB}}
-    {{LB}}two{{RB}}
-    {{LB}}three{{RB}}
-
-
-Default Renderer and customizing
---------------------------------
-
-* TODO
-
-
-Command line usage
-------------------
-
-* TODO
+Please see README.md for full documentation.
 
 """
 
