da6ebafec2da9eb77b4e707ffb0d8fff2244103f
Ben Hoyt <benhoyt@gmail.com>
2012-09-14 09:54:16 +1200

    Fix jinja2 benchmark, rework run_benchmark a bit

--- benchmarks/run_benchmarks.py
+++ benchmarks/run_benchmarks.py
@@ -1,6 +1,6 @@
 """Run all benchmarks."""
 
-# TODO: ensure compile() compiles included templates too
+# TODO: ensure compile() compiles included templates too, or at least that it's the same for everyone (render too)
 
 from __future__ import with_statement
 
@@ -12,34 +12,44 @@ import warnings
 
 BlogEntry = collections.namedtuple('BlogEntry', 'title url html_body')
 
-blog_entries = [
+TITLE = 'My Blog'
+ENTRIES = [
     BlogEntry(u'<Sorry>', u'/sorry/?a=b&c=d', u'<p>Sorry for the lack of updates.</p>'),
     BlogEntry(u'My life & story', None, u'<p>Once upon a time...</p>'),
     BlogEntry(u'First \u201cpost\u201d', u'/first-post/', u'<p>This is the first post.</p>'),
 ]
-blog_entries *= 10  # To give the render test a bit more to chew on
+ENTRIES *= 10  # To give the render test a bit more to chew on
 
 def rel_dir(dirname):
     """Return full directory name of dirname from this file's directory."""
     return os.path.abspath(os.path.join(os.path.dirname(__file__), dirname))
 
 class TemplateLanguage(object):
-    num_compiles = 100
+    num_compiles = 10
     num_renders = 100
 
-    def compile_main(self):
-        return self.compile('main')
+    def setup_compile(self):
+        pass
 
-    def render_main(self):
-        return self.render('main', title='My Blog', entries=blog_entries)
+    def compile(self):
+        raise NotImplementedError
+
+    def setup_render(self):
+        pass
+
+    def render(self):
+        raise NotImplementedError
 
     def benchmark(self):
         timings = {}
-        timings['compile'] = min(timeit.repeat(self.compile_main, number=self.num_compiles)) / float(self.num_compiles)
-        timings['render'] = min(timeit.repeat(self.render_main, number=self.num_renders)) / float(self.num_renders)
+        self.setup_compile()
+        timings['compile'] = min(timeit.repeat(self.compile, number=self.num_compiles)) / float(self.num_compiles)
+        self.setup_render()
+        timings['render'] = min(timeit.repeat(self.render, number=self.num_renders)) / float(self.num_renders)
         return timings
 
 try:
+    sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
     import symplate
 except ImportError:
     warnings.warn("Can't import symplate, is it in your PYTHONPATH?")
@@ -52,11 +62,11 @@ if symplate:
                     output_dir=rel_dir('symplate_output'),
                     modify_path=False)
 
-        def compile(self, name):
-            return self.renderer.compile(name)
+        def compile(self):
+            return self.renderer.compile('main')
 
-        def render(self, name, **kwargs):
-            return self.renderer.render(name, **kwargs)
+        def render(self):
+            return self.renderer.render('main', title=TITLE, entries=ENTRIES)
 
 try:
     import Cheetah.Template as cheetah
@@ -68,21 +78,19 @@ if cheetah:
     cheetah.checkFileMtime(False)
 
     class Cheetah(TemplateLanguage):
-        num_compiles = 10
-
         def __init__(self):
             self.template_dir = rel_dir('cheetah')
 
-        def compile(self, name):
-            file_name = os.path.join(self.template_dir, name + '.tmpl')
-            self.template = cheetah.Template.compile(
+        def compile(self):
+            file_name = os.path.join(self.template_dir, 'main.tmpl')
+            return cheetah.Template.compile(
                     file=file_name, cacheCompilationResults=False, useCache=False)
-            self.template_name = name
-            return self.template
 
-        def render(self, name, **kwargs):
-            assert name == self.template_name
-            params = dict(kwargs, template_dir=self.template_dir)
+        def setup_render(self):
+            self.template = self.compile()
+
+        def render(self):
+            params = dict(title=TITLE, entries=ENTRIES, template_dir=self.template_dir)
             return self.template(searchList=[params], filter=cheetah_websafe).respond()
 
 try:
@@ -94,15 +102,17 @@ if jinja2:
     class Jinja2(TemplateLanguage):
         def __init__(self):
             loader = jinja2.FileSystemLoader(rel_dir('jinja2'))
-            self.env = jinja2.Environment(loader=loader, autoescape=True)
+            self.compile_env = jinja2.Environment(loader=loader, autoescape=True, cache_size=0)
+            self.render_env = jinja2.Environment(loader=loader, autoescape=True)
 
-        def compile(self, name):
-            self.template = self.env.get_template('main.tmpl')
-            self.template_name = name
+        def compile(self):
+            return self.compile_env.get_template('main.tmpl')
 
-        def render(self, name, **kwargs):
-            assert name == self.template_name
-            return self.template.render(**kwargs)
+        def setup_render(self):
+            self.template = self.render_env.get_template('main.tmpl')
+
+        def render(self):
+            return self.template.render(title=TITLE, entries=ENTRIES)
 
 def main():
     language_classes = sorted((name, cls) for name, cls in globals().items()
@@ -113,9 +123,9 @@ def main():
         language = cls()
         timings = language.benchmark()
         print '%.20s %.3f %.3f' % (name, timings['compile'] * 1000, timings['render'] * 1000)
-        with open(name.lower() + '.html', 'w') as f:
-            language.compile_main()
-            f.write(language.render_main().encode('utf-8'))
+        with open(os.path.join(rel_dir(''), name.lower() + '.html'), 'w') as f:
+            language.setup_render()
+            f.write(language.render().encode('utf-8'))
 
 if __name__ == '__main__':
     main()
