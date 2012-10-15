a22b4e7500baaf813f5b6e21ed8564e6bd76bfa4
Ben Hoyt <benhoyt@gmail.com>
2012-10-15 23:13:56 +1300

    Tweak setup.py for PyPI release

--- setup.py
+++ setup.py
@@ -10,8 +10,17 @@ setup(
     author='Ben Hoyt',
     author_email='benhoyt@gmail.com',
     url='https://github.com/benhoyt/symplate',
-    license='LICENSE.txt',
+    license='New BSD License',
     description='Symplate, the Simple pYthon teMPLATE renderer',
-    long_description=open('README.md').read(),
+    long_description='Symplate is a very simple and very fast Python template language. Read more at the GitHub project page.',
     py_modules=['symplate'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ]
 )
